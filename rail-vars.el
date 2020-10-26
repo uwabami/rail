dae93f9d729bfc10558b0cf64b7ebb65367cf571
Youhei SASAKI <uwabami@gfd-dennou.org>
2020-10-26 21:13:32 +0900

    Bump Version: 1.2.13
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- rail-vars.el
+++ rail-vars.el
@@ -30,7 +30,7 @@
 (require 'rail-table-flim)
 (require 'rail-table-mule)
 
-(defvar rail-version "1.2.12"
+(defvar rail-version "1.2.13"
   "Version number of rail.el")
 
 (defvar rail-temporary-buffer-name " *Rail*"
